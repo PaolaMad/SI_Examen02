3fe6c0e9d12a3c0d37b89e84e375ac253e3bdf75
PaolaMad <paolamadridt@gmail.com>
2024-07-26 17:35:25 -0600

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rule: {
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react/no-unstable-nested-components': 'off',
+  },
 };
